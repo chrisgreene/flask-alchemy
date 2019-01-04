9c6d38d066d78efc0058d7b57082ca28f7208f8a
Chris Greene <chrisgreene@outlook.com>
2019-01-03 20:05:03 -0700

    Add uwsgi.ini

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os 
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -11,7 +12,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.debug = True
 app.secret_key = 'test'
@@ -35,4 +36,10 @@ api.add_resource(UserRegister, '/register')
 if __name__ == "__main__": 
   from db import db
   db.init_app(app)
+
+  if app.config['DEBUG']:
+    @app.before_first_request
+    def create_tables():
+      db.create_all()
+
   app.run()
